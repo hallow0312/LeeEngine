82f2077246ed67fe6ab90fb4e6d62928b508843c
hallow0312 <leejaehyun0312@naver.com>
2025-07-07 01:33:34 +0900

    Feat: 클래스다이어그램

--- Editor_Window/SichunEngine/SLoadResources.h
+++ Editor_Window/SichunEngine/SLoadResources.h
@@ -6,8 +6,8 @@ namespace Sichun
 {
 	void LoadResources()
 	{
-		Resources::Load<Graphics::Texture>(L"Reimu", L"C:\\Users\\user\\Desktop\\2DGame\\LeeEngine\\Editor_Window\\Resources\\Reimu\\Idle\\Reimu_Idle.png");
-		Resources::Load<Graphics::Texture>(L"BackGround", L"C:\\Users\\user\\Desktop\\2DGame\\LeeEngine\\Editor_Window\\Resources\\BackGround\\BackGround.png");
+		Resources::Load<Graphics::Texture>(L"Reimu", L"..\\Resources\\Reimu\\Idle\\Reimu_Idle.png");
+		Resources::Load<Graphics::Texture>(L"BackGround", L"..\\Resources\\BackGround\\BackGround.png");
 	}
 }
 
